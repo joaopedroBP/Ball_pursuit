549d9a9bc36d2aed83bfa30cdb00c9f4ba03c8de
João Pedro Bazoli palma <50723180+joaopedroBP@users.noreply.github.com>
2023-10-23 22:00:40 -0300

    Update Display.py

--- Display.py
+++ Display.py
@@ -1,22 +1,33 @@
 import pygame
-
+from ora_bolas import *
 pygame.init()
 
 altura_tela = 500
 largura_tela = 500
 
-tela = pygame.display.set_mode((altura_tela,largura_tela))
-robo = pygame.Rect((300,250,50,50))
 
 
-run = True
-while run:
-    pygame.draw.rect(tela,(255,0,0),robo)
+# posição inicial do robo
+x_robo = float(input("Enter the initial x position of the robot: "))
+y_robo = float(input("Enter the initial y position of the robot: "))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+robo = pygame.Rect((x_robo,y_robo,50,50))
 
-    pygame.display.update()
+tempo = temp(x_robo,y_robo)
 
-pygame.quit()
+if(tempo == None):
+    print("As cordenadas iniciais do robo tornam impossível a interceptação!")
+
+else:
+    tela = pygame.display.set_mode((altura_tela,largura_tela))
+    run = True
+    while run:
+        pygame.draw.rect(tela,(255,0,0),robo)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
+    pygame.quit()
