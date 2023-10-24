f60a9fedecab0890da9071c2e6c4741cedd5a31e
João Pedro Bazoli palma <50723180+joaopedroBP@users.noreply.github.com>
2023-10-23 22:01:02 -0300

    Update ora_bolas.py

--- ora_bolas.py
+++ ora_bolas.py
@@ -9,35 +9,36 @@ raio_interceptacao = 0.0943  # 9.43 em m
 def distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-# posição inicial do robo
-x_robo = float(input("Enter the initial x position of the robot: "))
-y_robo = float(input("Enter the initial y position of the robot: "))
+def temp(x_robo, y_robo):
 
-# lend o trajetoria da bola
-with open('ball_trajectory.txt', 'r') as file:
-    linhas = file.read().splitlines()
+    # lend o trajetoria da bola
+    with open('ball_trajectory.txt', 'r') as file:
+        linhas = file.read().splitlines()
 
-#substituir "." por "."
-for line in linhas[1:]:  
-    t, x_ball, y_ball = map(lambda val: float(val.replace(',', '.')), line.split())
+    #substituir "." por "."
+    for line in linhas[1:]:  
+        t, x_ball, y_ball = map(lambda val: float(val.replace(',', '.')), line.split())
     
-    # Calcular a distância entre o robo e um ponto da trajetoria da bola.
-    distance_to_ball = distance(x_robo, y_robo, x_ball, y_ball)
+        # Calcular a distância entre o robo e um ponto da trajetoria da bola.
+        distance_to_ball = distance(x_robo, y_robo, x_ball, y_ball)
     
-    # Calcular o tempo que o robo leva pra percorrer esse distância.
-    robot_time = math.sqrt((2 * distance_to_ball) / acelercao_robo)
+        # Calcular o tempo que o robo leva pra percorrer esse distância.
+        robot_time = math.sqrt((2 * distance_to_ball) / acelercao_robo)
     
-    if robot_time <= t:
-        print(f"Robot intercepts the ball at t = {t} seconds, x = {x_ball} meters, y = {y_ball} meters.")
-        break
-    else:
-        # Calculate the new position of the robot after t seconds
-        new_x_robo = x_robo + (t / 0.2) * velocidade_robo
-        new_y_robo = y_robo + (t / 0.2) * velocidade_robo
-        
-        # Calculate the distance between the new robot position and the ball's position at time t
-        new_distance_to_ball = distance(new_x_robo, new_y_robo, x_ball, y_ball)
-        
-        if new_distance_to_ball <= raio_interceptacao:
+        if robot_time <= t:
             print(f"Robot intercepts the ball at t = {t} seconds, x = {x_ball} meters, y = {y_ball} meters.")
-            break
+            return t
+        else:
+            # Calculate the new position of the robot after t seconds
+            new_x_robo = x_robo + (t / 0.2) * velocidade_robo
+            new_y_robo = y_robo + (t / 0.2) * velocidade_robo
+        
+            # Calculate the distance between the new robot position and the ball's position at time t
+            new_distance_to_ball = distance(new_x_robo, new_y_robo, x_ball, y_ball)
+        
+            if new_distance_to_ball <= raio_interceptacao:
+                print(f"Robot intercepts the ball at t = {t} seconds, x = {x_ball} meters, y = {y_ball} meters.")
+                return t;
+
+
+
